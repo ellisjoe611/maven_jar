5e3d3c61161c9d7ce1fe48d55f078b116aa28a07
Jong-Hyuk Ellis Cho <ellisjoe611@gmail.com>
2020-02-10 19:59:41 +0900

    reset
    reset

--- jdbc-practices/bookshop/schema/bookshop_create.sql
+++
@@ -1,4 +0,0 @@
-create database bookshop;
-create user 'bookshop'@'localhost' identified by 'bookshop';
-grant all privileges on bookshop.* to 'bookshop'@'localhost';
-flush privileges;
--- jdbc-practices/bookshop/schema/category_create.sql
+++
@@ -1,6 +0,0 @@
-insert into category values(null, '소설');
-insert into category values(null, '수필');
-insert into category values(null, '컴퓨터/IT');
-insert into category values(null, '인문');
-insert into category values(null, '경제');
-insert into category values(null, '예술');
